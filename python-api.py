9e3b3594588e1365f02098cbf20c9523b606ad76
zmohamed1905 <zakariya.m1905@gmail.com>
2022-10-17 15:11:55 +0100

    added python api

--- python-api.py
+++ python-api.py
@@ -1,1 +1,22 @@
 import requests
+
+request_bbc_status_code = requests.get("https://www.bbc.co.uk/iplayer/live/bbcnews")
+
+# Check the outcome of our api call
+
+if request_bbc_status_code.status_code == 200:
+    print("Successful")
+
+else:
+    print("Unsuccessful")
+
+
+
+
+# check the header
+
+# print(request_bbc_status_code.headers)
+
+# check the content available
+
+# print(request_bbc_status_code.content)
